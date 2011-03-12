c3a685c9e4e58bb7b280732f527144dc006d00b0
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-03-12 19:01:40 +0100

    some more code. still very trivial

--- autocollect-entities.py
+++ autocollect-entities.py
@@ -4,31 +4,49 @@ import os, os.path, fnmatch
 
 blacklistdirs = \
 	[
-	 os.path.expanduser("~/.*"),
-	 os.path.expanduser("~/Spiele"),
-	 os.path.expanduser("~/Applications"),
-	 os.path.expanduser("~/bin"),
-	 os.path.expanduser("~/build"),
-	 os.path.expanduser("~/Desktop"),
-	 os.path.expanduser("~/Downloads"),
-	 os.path.expanduser("~/gtk"),
-	 os.path.expanduser("~/Source"),
-	 os.path.expanduser("~/Wine Files"),
-	 os.path.expanduser("~/Public"),
+	 "~/.*",
+	 "~/Spiele",
+	 "~/Applications",
+	 "~/bin",
+	 "~/build",
+	 "~/Desktop",
+	 "~/Downloads",
+	 "~/gtk",
+	 "~/Source",
+	 "~/Wine Files",
+	 "~/Public",
 	 ]
-	
+
+categorydirs = \
+	[
+	 "~/Programmierung",
+	 "~/Documents",
+	 "~/Music",
+	 "~/Movies",
+	 "~/Library",
+	 "~/Library/Application Support",
+	 ]
+
 def find_in_dir(d):
 	for _d in blacklistdirs:
-		if fnmatch.fnmatch(d, _d): return
+		if fnmatch.fnmatch(d, os.path.expanduser(_d)): return
+	for _d in categorydirs:
+		if fnmatch.fnmatch(d, os.path.expanduser(_d)):
+			for subdir in os.listdir(d):
+				subdir = d + "/" + subdir
+				if not os.path.isdir(subdir): continue
+				for e in find_in_dir(subdir): yield e
+			return
+
 	yield { "name":os.path.basename(d), "dir":d }
+
 
 def find_all():
 	userdir = os.path.expanduser("~")
 	for d in os.listdir(userdir):
 		d = userdir + "/" + d
 		if not os.path.isdir(d): continue
-		for e in find_in_dir(d):
-			yield e
+		for e in find_in_dir(d): yield e
 
 if __name__ == "__main__":
 	for e in find_all(): print e
