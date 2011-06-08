0162ac0b73f97f82fa8bf0136d8003d40f06e6d9
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-06-08 16:17:13 +0200

    more on SimpleStruct

--- daemon.py
+++ daemon.py
@@ -12,18 +12,32 @@ def convert_unix_time(unixtime):
 	return datetime.utcfromtimestamp(unixtime).isoformat(" ")
 
 class SimpleStruct:
-	def __init__(self, **kwargs):
-		for k,v in kwargs.itervalues():
+	def __init__(self, *args, **kwargs):
+		if len(kwargs) == 0 and len(args) == 1 and type(args[0]) is dict:
+			kwargs = args[0]
+		for k,v in kwargs.iteritems():
 			setattr(self, k, v)
-			
-	def __repr__(self):
+	
+	def attribs(self):
 		stdobjattribs = set(dir(object()))
 		stdobjattribs.add("__module__")
 		attribs = []
 		for a in dir(self):
-			if a not in stdobjattribs: attribs += [a]
+			if a in stdobjattribs: continue
+			if callable(getattr(self, a)): continue
+			attribs += [a]		
+		return attribs
+	
+	def __repr__(self):
 		return self.__class__.__name__ + "(" + ", ".join(map(
-			lambda a: a + "=" + repr(getattr(self, a)), attribs)) + ")"
+			lambda a: a + "=" + repr(getattr(self, a)), self.attribs())) + ")"
+
+	def as_dict(self):
+		return dict(map(lambda a: (a, getattr(self, a)), self.attribs()))
+
+def json_encode(obj):
+	if isinstance(obj, SimpleStruct): return obj.as_dict()
+	raise TypeError, repr(obj) + " cannot be serialized"
 
 def get_db_obj(ref):
 	pass
